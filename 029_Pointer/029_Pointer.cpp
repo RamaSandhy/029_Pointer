0e99ea0815ba132e47a1022ce7ee4f8fbe730f59
RamaSandhy <rshandy07@gmail.com>
2023-05-10 15:15:57 +0700

    pointer

--- 029_Pointer/029_Pointer.cpp
+++ 029_Pointer/029_Pointer.cpp
@@ -10,3 +10,19 @@ public:
 void mahasiswa::showNim() { // implementasi method diluar class
 	cout << "No Induk = " << nim << endl;
 }
+
+int main()
+{
+	mahasiswa mhs{ 1 };             //object mhs
+	mhs.showNim();                  //Member Acces Operator
+
+	mahasiswa& ref = mhs;       //Pointer Reference refMhs
+	ref.nim = 2;                //Mmber Acces Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;     //Pointer Dereference pMhs
+	pMhs->nim = 3;
+	mhs.showNim();
+	system("pause");
+	return 0;
+}
